730b0bbab0d12fc42a842400a4cf409c402d0022
Steve Kavuu <mrkavuu@gmail.com>
2020-11-30 15:16:32 -0600

    bueno

---
+++ models/book.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const bookSchema = new Schema({
+    _id: { type: String },
+    title: { type: String },
+    authors: { type: Array },
+    description: { type: String },
+});
+
+const Book = mongoose.model("Book", bookSchema);
+
+module.exports = Book;
---
+++ server.js
@@ -0,0 +1,70 @@
+// Dependencies
+const express = require('express');
+const routes = require('./routes');
+
+// Express instance
+const app = express();
+
+// Variable Port
+const PORT = process.env.PORT || 9001;
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const mongoose = require('mongoose')
+
+mongoose.connect(
+    process.env.MONGODB_URI ||
+    "mongodb://localhost/googlebooks"
+);
+const bookModel = require('./models/book')
+
+// FIX FROM ONLINE FOR CORS ERRORS
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
+// If our node environment is production we will serve up our static assets from the build folder
+if (process.env.NODE_ENV === 'production') {
+    // The react app is called 'client' and we are accessing the build folder that is initialized in the postbuild scripts.
+    app.use(express.static('client/build'))
+};
+
+// API and View Routes
+
+app.post("/savedbooks", function(req,res){
+
+    var newBookToSave = {
+        title: req.body.volumeInfo.title,
+        authors: req.body.volumeInfo.authors,
+        description: req.body.searchInfo.textSnippet
+    }
+
+    bookModel.create(newBookToSave).then(function(data) {
+        console.log('new dude we saved!!', data)
+    })
+
+})
+
+// Start the server
+app.listen(PORT, () => {
+    if (process.env.NODE_ENV !== 'production') {
+        console.log(`Server listening at http://localhost:${PORT}`)
+    };
+});
